2a797cf1314a96e065050fb5f1c33d83ecbc3f56
Liam Brandt <lbrandt@umass.edu>
2019-04-02 13:04:40 -0400

    URL updates view, view updates URL.

--- app/main.tsx
+++ app/main.tsx
@@ -13,6 +13,12 @@ import CustomWindow = require("app/widgets/CustomWindow");
 import { CustomZoom, ZoomDirection } from "app/widgets/CustomZoom";
 import WindowExpand = require("app/widgets/WindowExpand");
 import { searchGoToOverride, searchSources } from "app/search";
+import {
+  homeGoToOverride,
+  resetUrlTimer,
+  umassLongLat,
+  updatePositionFromUrl
+} from "app/latLong";
 
 // Set the map to load data from our ArcGIS Online web map
 const map = new WebMap({
@@ -23,8 +29,8 @@ const map = new WebMap({
 
 const view = new MapView({
   container: "viewDiv",
-  // Start the map centered on UMass' latitude and longitude
-  center: [-72.5293, 42.3903],
+  // Start the map centered on UMass
+  center: umassLongLat,
   zoom: 16,
   map: map,
   // Tell the view to only load the attribution widget by default
@@ -32,6 +38,13 @@ const view = new MapView({
     components: ["attribution"]
   }
 });
+
+// Update the position of the view when the url hash changes
+updatePositionFromUrl(view);
+window.addEventListener("hashchange", () => { updatePositionFromUrl(view) });
+// Update the url hash when the position of the view changes
+view.watch("center", () => { resetUrlTimer(view) });
+view.watch("zoom", () => { resetUrlTimer(view) });
 
 // Wait until the view has loaded before loading the widgets
 view.when(() => {
@@ -98,7 +111,8 @@ view.when(() => {
       iconName: 'directions'
     }),
     home: new Home({
-      view: view
+      view: view,
+      goToOverride: homeGoToOverride
     }),
     layersExpand: new WindowExpand({
       name: 'layers',
