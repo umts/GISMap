f929fb12bfcd289432fa1ecaa73c9d969dfe8053
Liam Brandt <lbrandt@umass.edu>
2019-06-20 13:31:43 -0400

    RequestSet to ensure that sets of requests are made and applied in chronological order.

--- app/latLong.tsx
+++ app/latLong.tsx
@@ -1,7 +1,14 @@
 import Point = require("esri/geometry/Point");
+import Polygon = require('esri/geometry/Polygon');
 import MapView = require("esri/views/MapView");
 
 const umassLongLat = [-72.5293, 42.3903];
+
+// Represents a point on the screen in pixels
+interface ScreenPoint {
+  x: number;
+  y: number;
+}
 
 // Always go to center of UMass
 function homeGoToOverride(view: MapView, goToParams: any) {
@@ -16,7 +23,34 @@ function homeGoToOverride(view: MapView, goToParams: any) {
 }
 
 /*
+  Generate a 'circle' in latitude/longitude given a point on the screen in
+  pixels.
+*/
+function circleAt(screenPoint: ScreenPoint, view: MapView): Polygon {
+  const delta = 16;
+  const screenVertices: Array<ScreenPoint> = [
+    {x: screenPoint.x - delta, y: screenPoint.y - delta},
+    {x: screenPoint.x + delta, y: screenPoint.y - delta},
+    {x: screenPoint.x + delta, y: screenPoint.y + delta},
+    {x: screenPoint.x - delta, y: screenPoint.y + delta}
+  ]
+
+  const mapVertices = screenVertices.map((screenPoint) => {
+    const mapPoint = view.toMap(screenPoint);
+    /*
+      Explicitly grab lat/lon or else the polygon will try to convert back
+      to the MA spatial reference.
+    */
+    return [mapPoint.longitude, mapPoint.latitude];
+  });
+  let circle = new Polygon();
+  // Polygon ring requires the final point to be the same as the first
+  circle.addRing(mapVertices.concat([mapVertices[0]]));
+  return circle;
+}
+
+/*
   Export helper functions related to positioning so they can be
   imported and used in other files.
 */
-export { homeGoToOverride, umassLongLat };
+export { umassLongLat, ScreenPoint, homeGoToOverride, circleAt };
