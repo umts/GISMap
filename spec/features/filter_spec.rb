0942d7365828ca43d8e2f91ed706940b145fb45c
Liam Brandt <lbrandt@umass.edu>
2019-07-05 16:26:11 -0400

    Filter and popup specs

---
+++ spec/features/filter_spec.rb
@@ -0,0 +1,31 @@
+describe 'filter', type: :feature do
+  describe 'with no description' do
+    before :each do
+      expect(page).to have_no_selector('#filter-window')
+      fill_in 'main', with: 'Red lot'
+      first('.custom-search-suggestion').click
+      expect(page).to have_selector('#filter-window')
+    end
+
+    it 'filters by lot' do
+      expect(page).to have_content('Red Lots')
+    end
+
+    it 'closes' do
+      first('.custom-filter-close').click
+      expect(page).to have_no_selector('#filter-window')
+    end
+  end
+
+  describe 'with a description' do
+    it 'expands to show description' do
+      visitor_description = 'Pay at a meter or a paystation'
+
+      fill_in 'main', with: 'Metered visitor parking'
+      first('.custom-search-suggestion').click
+      expect(page).to have_no_content(visitor_description)
+      first('.expandable-filter').click
+      expect(page).to have_content(visitor_description)
+    end
+  end
+end
---
+++ spec/features/popup_spec.rb
@@ -0,0 +1,43 @@
+describe 'popup', type: :feature do
+  describe 'for lot 25' do
+    before :each do
+      visit_relative '#?latitude=42.39196&longitude=-72.53437&zoom=19.0'
+      find('#viewDiv').click
+    end
+
+    it 'opens and closes' do
+      expect(page).to have_content('Lot 25 (Green)')
+      click_by_title 'Close feature information'
+      expect(page).to have_no_content('Lot 25 (Green)')
+    end
+
+    it 'docks and undocks' do
+      # Should start docked
+      expect(page).to have_selector('.custom-popup-container.docked')
+      click_by_title 'Un-dock feature information'
+      expect(page).to have_selector('.custom-popup-container')
+      expect(page).to have_no_selector('.custom-popup-container.docked')
+      click_by_title 'Dock feature information'
+      expect(page).to have_selector('.custom-popup-container.docked')
+    end
+  end
+
+  describe 'multiple features' do
+    it 'loops around to first feature and back' do
+      visit_relative '#?latitude=42.39193&longitude=-72.53521&zoom=17.0'
+
+      find('#viewDiv').click
+      expect(page).to have_content('Lot 12 (Yellow)')
+      expect(page).to have_content('1/2')
+      click_button 'Next'
+      expect(page).to have_content('Lot 25 (Green)')
+      expect(page).to have_content('2/2')
+      click_button 'Next'
+      expect(page).to have_content('Lot 12 (Yellow)')
+      expect(page).to have_content('1/2')
+      click_button 'Previous'
+      expect(page).to have_content('Lot 25 (Green)')
+      expect(page).to have_content('2/2')
+    end
+  end
+end
